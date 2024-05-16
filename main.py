19254cb7c94e5fbd5a5d1b70568e284bd65bb792
PragatiVerma18 <itispragativerma@gmail.com>
2024-05-16 18:36:11 +0530

    Add langchain chat app

---
+++ main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+st.set_page_config(page_title="Streaming Bot", page_icon="🤖")
+
+st.title("Streaming Bot")
+
+
+# Get AI Response
+def get_response(query, chat_history):
+    template = """
+    You are a helpful assistant. Answer the following questions considering the chat history.
+    
+    Chat history: {chat_history}
+    
+    User question: {user_question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+    llm = ChatOpenAI()
+    chain = prompt | llm | StrOutputParser()
+    return chain.stream({
+        "chat_history": chat_history,
+        "user_question": query,
+    })
+
+
+# Conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+    else:
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+
+# User Input
+user_query = st.chat_input("Your message")
+if user_query is not None and user_query != "":
+    st.session_state.chat_history.append(HumanMessage(user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+    with st.chat_message("AI"):
+        ai_response = st.write_stream(get_response(user_query, st.session_state.chat_history))
+
+    st.session_state.chat_history.append(AIMessage(ai_response))
